76a4127c23568c6ebe233fc9593d34f4ace97b28
wmwolf <wmwolf@physics.ucsb.edu>
2014-07-10 19:29:08 -0700

    Change navbar and header theme. Add Research text.

--- js/application.js
+++ js/application.js
@@ -1,67 +1,75 @@
-function viewport()
-{
-  var viewportwidth;
-  var viewportheight;
-  
-  // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-  
-  if (typeof window.innerWidth != 'undefined')
-  {
-       viewportwidth = window.innerWidth,
-       viewportheight = window.innerHeight
-  }
-  
- // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
- 
-  else if (typeof document.documentElement != 'undefined'
-      && typeof document.documentElement.clientWidth !=
-      'undefined' && document.documentElement.clientWidth != 0)
-  {
-        viewportwidth = document.documentElement.clientWidth,
-        viewportheight = document.documentElement.clientHeight
-  }
-  
-  // older versions of IE
-  
-  else
-  {
-        viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
-        viewportheight = document.getElementsByTagName('body')[0].clientHeight
-  }
-  return viewportwidth
-}
+var img_dimensions = function(klass) {
+  var image_url = $('.' + klass).css('background-image'),
+      image,
+      width,
+      height,
+      res;
+      
+  // Remove url() or in case of Chrome url("")
+  image_url = image_url.match(/^url\("?(.+?)"?\)$/);
+
+  if (image_url[1]) {
+      image = new Image();
+      image_url = image_url[1];
+      // just in case it is not already loaded
+      image.src = image_url;
+      // res = $(image).load(function() {
+      width = image.width
+      height = image.height
+      return([width, height]);
+      // });
+      // alert("res is " + res)
+      // return res
+  };
+};
 
 $( document ).ready(function() {
-  if (viewport() > 768)
-  {
-    boxes = $('.panel').not(".no-height-fix");
-    maxHeight = Math.max.apply(
-    Math, boxes.map(function() {
-        return $(this).height();
-    }).get());
-    boxes.height(maxHeight);
-  }
-});
+  var bckgrd_options = ['bckgrd-1', 'bckgrd-2', 'bckgrd-3', 'bckgrd-4',
+                        'bckgrd-5'];
+  var bckgrd_number = Math.floor(Math.random() * bckgrd_options.length);
+  var bckgrd_class;
+	var splash = $(".splash");
+  bckgrd_class = bckgrd_options[bckgrd_number];
+  splash.toggleClass(bckgrd_class);
+  
+  // var image_url = $('.' + bckgrd_class).css('background-image'),
+  //     image,
+  //     width,
+  //     height,
+      
+  // Remove url() or in case of Chrome url("")
+  // image_url = image_url.match(/^url\("?(.+?)"?\)$/);
+  // if (image_url[1]) {
+  //   var speed = 2.0;
+  //
+  //   image = new Image();
+  //   image_url = image_url[1];
+    
+    // just in case it is not already loaded
+    // image.src = image_url;
+    //
+    // $(image).load(function() {
+    //   width = image.width
+    //   height = image.height
+    //   $(window).scroll(function () {
+        // alert("background_width = " + width)
+        // var aspect_ratio = splash.width() / width;
+        // var scaled_height = aspect_ratio * height;
+        // var yPos = -$(window).scrollTop() / speed;
+        // var pos = "50% " + yPos + "px";
+        // alert("scaled height = " + scaled_height);
+        // if (yPos * speed + scaled_height > splash.height()) {
+          // alert(yPos + scaled_height + " > " + splash.height())
+  //         splash.css('background-position', pos);
+  //       };
+  //     });
+  //
+  //   });
+  // };
+    
+  // var dimensions = img_dimensions(bckgrd_class);
+  // var background_width = dimensions[0];
+  // var background_height = dimensions[1];
+  // alert("width = " + background_width + "; height = " + background_height)
+}); 
 
-$( window ).resize(function() {
-  boxes = $('.panel').not(".no-height-fix");
-  boxes.height("auto")
-  if (viewport() > 768)
-  {
-    maxHeight = Math.max.apply(
-    Math, boxes.map(function() {
-        return $(this).height();
-    }).get());
-    boxes.height(maxHeight);
-  }
-});
-$( document ).ready(function() {
-  // expand recent papers box if not expanded, contract it otherwise
-  $('.expandable').on('click', function(event) {
-    event.preventDefault();
-    $(this).closest(".panel").find(".toggle").slideToggle();
-    $(this).find('span').toggleClass("glyphicon-chevron-down");
-    $(this).find('span').toggleClass("glyphicon-chevron-up")   ; 
-  });
-}
-);
