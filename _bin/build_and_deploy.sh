b384cdcbf3d74285d71c08a1d40cdeefc98f4f02
Bill Wolf <wolfwm@uwec.edu>
2020-08-08 15:33:27 -0500

    wipe slate before splitting source and hosting repos

--- _bin/build_and_deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-/usr/bin/env ruby ~/Repositories/wmwolf.github.io/_bin/gen_resources.rb student Blu1sMyHomeboy covid-19
-bundle exec jekyll build --destination _site
-git add --all
-git commit -am "Update resources, username, password, and code."
-git push
